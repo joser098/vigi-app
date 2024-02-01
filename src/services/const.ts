47c27f723e0885e5ef0b5ca15f1c9f6c02d687e7
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-02-01 19:45:35 -0300

    Add quantity constants

---
+++ src/services/const.ts
@@ -0,0 +1,32 @@
+export const quantity = [
+    {
+        id: 1,
+        value: 1,
+        descripcion: '1 Unidad'
+    },
+    {
+        id: 2,
+        value: 2,
+        descripcion: '2 Unidades'
+    },
+    {
+        id: 3,
+        value: 3,
+        descripcion: '3 Unidades'
+    },
+    {
+        id: 4,
+        value: 4,
+        descripcion: '4 Unidades'
+    },
+    {
+        id: 5,
+        value: 5,
+        descripcion: '5 Unidades'
+    },
+    {
+        id: 5,
+        value: 5,
+        descripcion: '5 Unidades'
+    }
+];
