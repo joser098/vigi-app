6b1a3d482cbb7a5c45e238b90c31f10674918981
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-02-10 16:26:11 -0300

    Add Footer component to Layout and update index page

--- src/services/const.ts
+++ src/services/const.ts
@@ -68,4 +68,113 @@ export const faq = [
         question: '¿Qué es Lorem Ipsum?',
         answer: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.'
     }
-]
+];
+
+export const footerData = [
+    {
+        id: 1,
+        title: 'Acerca de Nosotros',
+        redirects: [
+            {
+                id: 1,
+                title: 'Quiénes Somos',
+                url: '/nosotros'
+            },
+            {
+                id: 2,
+                title: 'Nuestros Valores',
+                url: '/valores'
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: 'Envíos y Devoluciones',
+        redirects: [
+            {
+                id: 1,
+                title: 'Política de Envíos',
+                url: '/envios'
+            },
+            {
+                id: 2,
+                title: 'Política de Devoluciones',
+                url: '/devoluciones'
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: 'Soporte',
+        redirects: [
+            {
+                id: 1,
+                title: 'Preguntas Frecuentes',
+                url: '/#faq'
+            },
+            {
+                id: 2,
+                title: 'Contacto',
+                url: '/contacto'
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: 'Redes Sociales',
+        redirects: [
+            {
+                id: 1,
+                title: 'Facebook',
+                url: 'https://www.facebook.com'
+            },
+            {
+                id: 2,
+                title: 'Instagram',
+                url: 'https://www.instagram.com/vigi.cam_'
+            },
+            {
+                id: 3,
+                title: 'Whatsapp',
+                url: 'https://api.whatsapp.com/send?phone=541126039243&text=Hola!.%20Me%20gustaria%20saber%20m%C3%A1s!'
+            }
+        ]
+    },
+    {
+        id: 5,
+        title: 'Legal',
+        redirects: [
+            {
+                id: 3,
+                title: 'Términos y Condiciones',
+                url: '/terminos'
+            },
+            {
+                id: 4,
+                title: 'Política de Privacidad',
+                url: '/privacidad'
+            },
+        ]   
+    },
+    {
+        id: 6,
+        title: 'Navegar',
+        redirects: [
+            {
+                id: 1,
+                title: 'Inicio',
+                url: '/'
+            },
+            {
+                id: 2,
+                title: 'Carrito',
+                url: '/cart'
+            },
+            {
+                id: 3,
+                title: 'Perfil',
+                url: '/perfil'
+            }
+        ]
+    }
+];
