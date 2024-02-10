3e8b20157553574e40bd048f06c516371dd69835
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-02-10 01:14:28 -0300

    Add CartButton component and Toast message

--- src/components/AddCartButton.tsx
+++ src/components/AddCartButton.tsx
@@ -1,7 +1,12 @@
-import type { Product } from "@/services/types";
+import type { CartItem, Product } from "@/services/types";
 import { addToCart, getQuantity } from "@/store/cartStore";
+import Toast from "./Toast";
+import { useState } from "react";
 
 const AddCartButton = ({ product }: { product: Product }) => {
+  const [showToast, setShowToast] = useState(false);
+  const [item, setitem] = useState<CartItem>({} as CartItem);
+
   const onAddCartClick = async () => {
     const quantity = getQuantity(product._id);
     const item = {
@@ -11,15 +16,25 @@ const AddCartButton = ({ product }: { product: Product }) => {
       unit_price: product.price,
     };
     const res = await addToCart(item);
+    if(res.success){
+      setitem(item)
+      setShowToast(true);
+      setTimeout(() => {
+        setShowToast(false);
+      }, 5000);
+    }
   };
 
   return (
-    <button
-      onClick={onAddCartClick}
-      className="block w-full bg-violet-100 border-2 border-violet-100 text-primary p-3 rounded-md hover:opacity-75 transition-opacity"
-    >
-      Agregar al carrito
-    </button>
+    <>
+      <button
+        onClick={onAddCartClick}
+        className="block w-full bg-violet-100 border-2 border-violet-100 text-primary p-3 rounded-md hover:opacity-75 transition-opacity"
+      >
+        Agregar al carrito
+      </button>
+      {showToast && <Toast title="Producto Agregado!" message={`Se ha agregado ${item.quantity} ${item.quantity == 1 ? "unidad" : "unidades"} de ${item.title} al carrito`} />}
+    </>
   );
 };
 
