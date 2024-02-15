d1275d88a710b9be3a5cba79876b53a708195bb9
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-02-15 17:45:27 -0300

    validate pay button to not make request with 0 items in cart

--- src/components/PayCartButton.tsx
+++ src/components/PayCartButton.tsx
@@ -1,24 +1,35 @@
 import { createPaymentOrder } from "@/services/fetchData";
 import type { CartModel } from "@/services/types";
+import { useEffect, useState } from "react";
 
 const PayCartButton = ({ cart }: {cart: CartModel}) => {
-  const onPayCartClick = async () => {
-    const cartModel = {
-      _id: cart._id,
-      customer_id: cart.customer_id,
-      items: cart.items,
-      products_total:cart.products_total,
-      amount_to_pay: cart.products_total,
-    }
+   const [isEnable, setIsEnable] = useState(true);
 
-    const res = await createPaymentOrder(cartModel);
-    if(res.success){
-      window.location.href = res.data.init_point;
+  const onPayCartClick = async () => {
+    if(cart.items.length > 0){
+      const cartModel = {
+        _id: cart._id,
+        customer_id: cart.customer_id,
+        items: cart.items,
+        products_total:cart.products_total,
+        amount_to_pay: cart.products_total,
+      }
+  
+      const res = await createPaymentOrder(cartModel);
+      if(res.success){
+        window.location.href = res.data.init_point;
+      }
     }
   };
 
+  useEffect(() => {
+    if(cart.items.length > 0){
+      setIsEnable(false)
+    }
+  }, [])
+
   return (
-    <button onClick={onPayCartClick} className="w-full bg-primary border-2 border-primary text-white p-3 rounded-md hover:opacity-70 transition-opacity my-3">
+    <button onClick={onPayCartClick} disabled={isEnable} className="w-full bg-primary border-2 border-primary text-white p-3 rounded-md hover:opacity-70 transition-opacity my-3">
       Pagar
     </button>
   );
