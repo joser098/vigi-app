96958e0e527da85f80dee108659765e65cc3e29e
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-02-20 23:54:10 -0300

    Update FAQ's

--- src/services/const.ts
+++ src/services/const.ts
@@ -35,38 +35,43 @@ export const quantity = [
 export const faq = [
     {
         id: 1,
-        question: '¿Qué es Lorem Ipsum?',
-        answer: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.'
+        question: '¿Cuáles son los métodos de pago aceptados?',
+        answer: 'Nuestra tienda procesa los pagos de manera segura con Mercado Pago y acepta los siguientes métodos de pago: tarjetas de crédito (Visa, Mastercard, American Express), tarjetas de débito. Tambien mediante transferencia bancaria.'
     },
     {
         id: 2,
-        question: '¿Qué es Lorem Ipsum?',
-        answer: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.'
+        question: '¿Cuánto tiempo tarda en llegar mi pedido?',
+        answer: 'El tiempo de entrega varía según tu ubicación y el método de envío seleccionado. Por lo general, los pedidos tardan entre algunas horas y 2 o 3 días hábiles en llegar después de haber sido despachados.'
     },
     {
         id: 3,
-        question: '¿Qué es Lorem Ipsum?',
-        answer: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.'
+        question: '¿Tienen servicio de atención al cliente? ¿Cómo puedo contactarlos?',
+        answer: 'Sí, tenemos un equipo de atención al cliente disponible para ayudarte con cualquier pregunta o inquietud. Puedes contactarnos por correo electrónico a contacto@vigi.cam o por WhatsApp al 11 2603 9243.'
     },
     {
         id: 4,
-        question: '¿Qué es Lorem Ipsum?',
-        answer: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.'
+        question: '¿Cómo puedo rastrear mi pedido?',
+        answer: 'Una vez que tu pedido haya sido despachado, recibirás un correo electrónico. Puedes iniciar sesión en tu cuenta y ver el estado de tu pedido en la sección "Pedidos".'
     },
     {
         id: 5,
-        question: '¿Qué es Lorem Ipsum?',
-        answer: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.'
+        question: '¿Puedo modificar o cancelar mi pedido después de realizarlo?',
+        answer: 'Si necesitas modificar o cancelar tu pedido, contáctanos lo antes posible. Haremos todo lo posible para ayudarte, pero ten en cuenta que una vez que el pedido haya sido procesado y despachado, es posible que no podamos realizar cambios.'
     },
     {
         id: 6,
-        question: '¿Qué es Lorem Ipsum?',
-        answer: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.'
+        question: '¿Tienen alguna opción de envío express? ¿Cuál es su costo?',
+        answer: 'Sí, ofrecemos opciones de envío express con tarifas y tiempos de entrega variables según la ubicación. Puedes ver las opciones disponibles durante el proceso de compra antes de finalizar tu pedido.'
     },
     {
         id: 7,
-        question: '¿Qué es Lorem Ipsum?',
-        answer: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.'
+        question: '¿Ofrecen garantía en sus productos?',
+        answer: 'Ofrecemos garantía en todos nuestros productos contra defectos de fabricación en un período entre 6 meses y 2 años dependiendo de la marca. Si experimentas algún problema con tu producto dentro de este período, por favor contáctanos para obtener asistencia.'
+    },
+    {
+        id: 8,
+        question: '¿Tienen una tienda física donde pueda ver los productos personalmente?',
+        answer: 'Actualmente operamos exclusivamente como un e-commerce y no tenemos tiendas físicas. Sin embargo, puedes ver fotos detalladas y descripciones de nuestros productos en nuestro sitio web. Puedes contactarnos por correo electrónico a contacto@vigi.cam o por WhatsApp al 11 2603 9243.'
     }
 ];
 
