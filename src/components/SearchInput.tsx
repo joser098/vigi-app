48e78736217b01cc50e683f397acb8e071fe64ca
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-05-10 18:29:04 -0300

    Refactor search input ui

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -8,6 +8,10 @@ interface Item {
   provider: string;
   model: string;
   price: number;
+  price_diferred: number;
+  has_promotion: boolean;
+  price_original: number;
+  discount: number;
 }
 
 const SearchInput = () => {
@@ -24,7 +28,7 @@ const SearchInput = () => {
     }
     if (value.length > 1) {
       //Hacer request para suggest
-      const products = await searchSuggest(value, 6);
+      const products = await searchSuggest(value, 10);
 
       //Agregar los resultados al suggeslist
       setSuggestList(products);
@@ -32,41 +36,75 @@ const SearchInput = () => {
   };
 
   const onKeyDown = async (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if(e.key == "Enter"){
-      navigate(`/buscar?search=${inputValue}`)
+    if (e.key == "Enter") {
+      navigate(`/buscar?search=${inputValue}`);
     }
+  };
+
+  const onBlurHandler = () => {
+    setSuggestList([]);
   };
 
   return (
     <div className="text-xs relative">
       <input
-      onKeyDown={onKeyDown}
+        onKeyDown={onKeyDown}
         onChange={onChangeSearch}
+        onBlur={onBlurHandler}
         className="hidden w-96 sm:inline border-[1px] border-primary rounded py-[10px] pl-1 pr-6"
         type="text"
         placeholder="Buscar producto, marca ..."
       />
-      <section className="absolute w-full bg-white mt-1 rounded">
+      <section className="absolute w-full bg-white mt-1 rounded max-h-[280px] overflow-y-auto">
         {suggestList?.map((item: Item) => {
           return (
-            <article key={item._id}>
-              <a href={`/product/${item.model}`} className="flex hover:bg-violet-100 rounded">
+            <article
+              key={item._id}
+              className="relative hover:bg-gray-100 rounded py-2"
+            >
+              {item.has_promotion && (
+                <div className="absolute w-1/3 flex flex-col  justify-center items-center top-6 right-0 rounded-l gradient_violet bg-gradient-to-tr">
+                  <span className="text-white text-[10px]">Ahorras</span>
+                  <span className="text-white text-sm font-bold">
+                    {item.price_diferred?.toLocaleString("es-AR", {
+                      style: "currency",
+                      currency: "ARS",
+                      maximumFractionDigits: 0,
+                    })}
+                  </span>
+                </div>
+              )}
+              <a href={`/product/${item.model}`} className="flex rounded">
                 <img
                   src={item.thumbnail}
                   alt={item.model}
                   className="max-h-16 max-w-16"
                 />
                 <div className="flex flex-col justify-center gap-1">
-                  <h6 className="text-lg font-bold text-primary">
+                  <h6 className="text-sm font-bold text-primary">
                     {item.model}
                   </h6>
-                  <span>
-                    {item.price.toLocaleString("es-AR", {
-                      style: "currency",
-                      currency: "ARS",
-                      minimumFractionDigits: 0,
-                    })}
-                  </span>
+                  <div className="flex items-center gap-1">
+                    <span className="text-sm font-bold">
+                      {item.price.toLocaleString("es-AR", {
+                        style: "currency",
+                        currency: "ARS",
+                        minimumFractionDigits: 0,
+                      })}
+                    </span>
+                    {item.has_promotion && (
+                      <span className="text-green_ text-[10px]">{item.discount}% OFF</span>
+                    )}
+                  </div>
+                  {item.has_promotion && (
+                    <p className="text-xs line-through bg-gray-200 text-gray-400 rounded-full px-2 max-w-fit">
+                      {item.price_original?.toLocaleString("es-AR", {
+                        style: "currency",
+                        currency: "ARS",
+                        minimumFractionDigits: 0,
+                      })}
+                    </p>
+                  )}
                 </div>
               </a>
             </article>
