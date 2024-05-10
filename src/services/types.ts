40ad720efd29c09cd73c0b81b531730487127fa1
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-05-10 03:55:02 -0300

    Refactor types

--- src/services/types.ts
+++ src/services/types.ts
@@ -11,6 +11,7 @@ export interface Product {
   price: number;
   price_original: number;
   price_diferred: number;
+  discount: number; 
   details: CameraDetails;
   dvr_details: DvrDetails;
   has_promotion: boolean;
