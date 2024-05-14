ec39217aa39036c026b009d1fc625366b182f095
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-05-14 03:43:04 -0300

    Fix navigation after delete item && empty cart

--- src/components/Buttons/DeleteItemButton.tsx
+++ src/components/Buttons/DeleteItemButton.tsx
@@ -1,11 +1,12 @@
 import { removeItemCart } from "@/store/cartStore";
 import Delete from "../Icons/Delete";
+import { navigate } from "astro/virtual-modules/transitions-router.js";
 
 const DeleteItemButton = ({ product_id }: { product_id: string }) => {
     const onDeleteClick = async () => {
         const res = await removeItemCart(product_id);
         if(res.success){
-            window.location.reload(); // change this to a state update
+            navigate("/cart")
         }
     };
 
