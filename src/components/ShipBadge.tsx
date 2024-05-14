99dd3300723efadc3e64da25c79b9f9a427bbaf3
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-05-14 01:08:20 -0300

    Add make shipbadge dynamic

---
+++ src/components/ShipBadge.tsx
@@ -0,0 +1,24 @@
+import { getTime } from "@/services/scripts";
+import { useEffect, useState } from "react";
+
+const ShipBadge = () => {
+    const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    const time = getTime();
+    setTime(time)
+  });
+
+  return (
+    <div className="my-2 flex justify-center items-center">
+      <span
+        id="ship_date"
+        className="text-[12px] py-1 px-2 rounded-full gradient text-green_"
+      >
+        {time >= 0 && time < 16 ? "Llega hoy (CABA)" : "Llega mañana (CABA)"}
+      </span>
+    </div>
+  );
+};
+
+export default ShipBadge;
