7cd470b4ffd2e19b3152685aebbf97bff40baffb
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-06-03 10:17:43 -0300

    change styles to faq

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -14,15 +14,15 @@ const Faq = () => {
         {
             faq.map((faq: FaqItem) => {
                 return (
-                    <Accordion.Item value={`item-${faq.id}`} key={faq.id} className=" odd:bg-gray-100 p-1 focus-within:shadow-primary mt-px overflow-hidden first:mt-0 first:rounded-t last:rounded-b focus-within:relative focus-within:z-10 focus-within:shadow-[0_0_0_2px]">
+                    <Accordion.Item value={`item-${faq.id}`} key={faq.id} className="odd:bg-gray-100 p-1 focus-within:shadow-primary mt-px overflow-hidden first:mt-0 first:rounded-t last:rounded-b focus-within:relative focus-within:z-10 focus-within:shadow-[0_0_0_2px]">
                         <Accordion.AccordionTrigger className="w-full text-primary group flex h-[45px] flex-1 cursor-default items-center justify-between px-5 text-[15px] leading-none outline-none'">
-                            <h2 className="font-semibold">{faq.question}</h2>
+                            <h2 >{faq.question}</h2>
                             {/*TODO: Rotate only when click */}
                             <div className="transform transition-transform duration-500 group-hover:rotate-180">
                                 <ChevronDown/>
                             </div>
                         </Accordion.AccordionTrigger>
-                        <Accordion.AccordionContent className=" text-gray-600 data-[state=open]:animate-slideDown data-[state=closed]:animate-slideUp overflow-hidden text-xs px-5">
+                        <Accordion.AccordionContent className=" text-gray-600 data-[state=open]:animate-slideDown data-[state=closed]:animate-slideUp overflow-hidden text-xs px-5 py-2 sm:py-0 ">
                             <p>{faq.answer}</p>
                         </Accordion.AccordionContent>
                     </Accordion.Item>
