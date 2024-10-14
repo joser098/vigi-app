cc172d84408359d333714a50cff09818e2765a45
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2024-10-14 18:15:07 -0300

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  site: 'https://www.vigi.cam',
+  integrations: [tailwind(), react(), sitemap()],
   output: "hybrid",
   adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
+    webAnalytics: {
+      enabled: true
+    }
+  })
 });
